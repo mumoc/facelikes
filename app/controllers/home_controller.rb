4445835aca043d9f18494d554e7f9a1b648adec4
Carlos Muñiz <carlos.muniz@crowdint.com>
2011-07-18 00:16:09 -0500

    pruebanding

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,42 +13,43 @@ class HomeController < ApplicationController
 
     all_albums.each do |album|
       if interested_albums.include? album.identifier
-        photos_with_likes = []
-        photos = album.photos
-        phot = 1
-        (1..8).each do
-          if phot != 0
-            unless photos.empty?
-              photos.each do |p|
-                likes = p.likes
-                count = 0
-                like = 1
-
-                (1..10).each do
-                  if like != 0
-                    unless likes.empty?
-                      count = count + likes.count
-                    end
-                    likes = likes.next
-                    if likes.empty?
-                      like = 0
-                    end
-                  end
-                end
-
-                if count > 0
-                  photo = {:photo_id => p.identifier, :likes_count => count, :link => p.link}
-                  photos_with_likes << photo
-                end
-              end
-            end
-            photos = photos.next
-            if photos.empty?
-              phot = 0
-            end
-          end
-        end
-        @album_photos << {:album_id => album.identifier, :album_name => album.name, :photo => photos_with_likes}
+#        photos_with_likes = []
+#        photos = album.photos
+#        phot = 1
+#        (1..8).each do
+#          if phot != 0
+#            unless photos.empty?
+#              photos.each do |p|
+#                likes = p.likes
+#                count = 0
+#                like = 1
+#
+#                (1..10).each do
+#                  if like != 0
+#                    unless likes.empty?
+#                      count = count + likes.count
+#                    end
+#                    likes = likes.next
+#                    if likes.empty?
+#                      like = 0
+#                    end
+#                  end
+#                end
+#
+#                if count > 0
+#                  photo = {:photo_id => p.identifier, :likes_count => count, :link => p.link}
+#                  photos_with_likes << photo
+#
+#                end
+#              end
+#            end
+#            photos = photos.next
+#            if photos.empty?
+#              phot = 0
+#            end
+#          end
+#        end
+        @album_photos << {:album_id => album.identifier, :album_name => album.name}
       end
     end
   end
