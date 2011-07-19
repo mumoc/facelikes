7e0534c3c32a379fef1bd37c973ece5e07043b81
Carlos Muñiz <carlos.muniz@crowdint.com>
2011-07-18 19:52:34 -0500

    topten

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,45 @@
 class HomeController < ApplicationController
+#  def index
+#    config = YAML::load(File.open("#{RAILS_ROOT}/config/facebook.yml"))
+#    my_app = FbGraph::Application.new(config['production']['app_id'])
+#    acc_tok = my_app.get_access_token(config['production']['client_secret'])
+#    @page = FbGraph::Page.new(config['production']['page_id'], :access_token => acc_tok)
+#
+#    all_albums = @page.albums(:fields => "id,name")
+#    interested_albums = ['241486485875905','240961662595054','240424745982079']
+#
+#    @album_photos = []
+#
+#    all_albums.each do |album|
+#      if interested_albums.include? album.identifier
+#        photos_with_likes = []
+#        photos = album.photos(:fields => "id,link", :limit => 201)
+#        unless photos.empty?
+#          photos.each do |p|
+#            count = 0
+#            likes = p.likes(:fields => "id",:limit => 200)
+#            count = likes.size unless likes.empty?
+#            if count > 0
+#              photo = {:photo_id => p.identifier, :likes_count => count, :link => p.link}
+#              photos_with_likes << photo
+#            end
+#            photos_with_likes.sort! {|a,b| a[:likes_count] <=> b[:likes_count]}.reverse!
+#          end
+#        end
+#        @album_photos << {:album_id => album.identifier, :album_name => album.name, :photo => photos_with_likes}
+#
+#      end
+#    end
+#  end
+
   def index
-    config = YAML::load(File.open("#{RAILS_ROOT}/config/facebook.yml"))
-    my_app = FbGraph::Application.new(config['production']['app_id'])
-    acc_tok = my_app.get_access_token(config['production']['client_secret'])
-    @page = FbGraph::Page.new(config['production']['page_id'], :access_token => acc_tok)
-
-    all_albums = @page.albums(:fields => "id,name")
-    interested_albums = ['241486485875905','240961662595054','240424745982079']
-
-    @album_photos = []
-
-    all_albums.each do |album|
-      if interested_albums.include? album.identifier
-        photos_with_likes = []
-        photos = album.photos(:fields => "id,link", :limit => 201)
-        unless photos.empty?
-          photos.each do |p|
-            count = 0
-            likes = p.likes(:fields => "id",:limit => 200)
-            count = likes.size unless likes.empty?
-            if count > 0
-              photo = {:photo_id => p.identifier, :likes_count => count, :link => p.link}
-              photos_with_likes << photo
-            end
-            photos_with_likes.sort! {|a,b| a[:likes_count] <=> b[:likes_count]}.reverse!
-          end
-        end
-        @album_photos << {:album_id => album.identifier, :album_name => album.name, :photo => photos_with_likes}
-      end
+    top = ["240435805980973"]
+    @top_ten = []
+    top.each do |photo|
+      p = FbGraph::Photo.fetch(photo,:fields => "id,link")
+      count = p.likes(:limit => 200).count
+      photo_with_likes = {:photo_id => p.identifier, :link => p.link, :likes_count => count }
+      @top_ten << photo_with_likes
     end
   end
 end
